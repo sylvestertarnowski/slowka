2429ec0c75510f5747df004bc5b985d67995a8a7
Sylvester <34459277+sylvestertarnowski@users.noreply.github.com>
2019-11-10 16:13:50 +0100

    add App routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import './App.css';
-import Menu from './components/menu/Menu';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Menu from "./components/menu/Menu";
 
 function App() {
   return (
     <div className="App">
-      <Menu />
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Menu />
+          </Route>
+          <Route path="/create">
+            {/* <Create /> */}
+          </Route>
+          <Route path="/learn">
+            {/* <Learn /> */}
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
