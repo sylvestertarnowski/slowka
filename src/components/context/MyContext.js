f7439b5008765c540e7a3b95e419dce2e786cd9c
Sylvester <34459277+sylvestertarnowski@users.noreply.github.com>
2019-11-10 15:51:05 +0100

    add context store

---
+++ src/components/context/MyContext.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+const MyContext = React.createContext({});
+
+export class MyProvider extends Component {
+  state = {
+    collections: [],
+    language: "pl",
+    newCollection: []
+  };
+  render() {
+    return (
+      <MyContext.Provider
+        value={{
+          state: this.state
+        }}
+      >
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+};
+
+export default MyContext;
