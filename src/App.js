3f9ca2d1b231ebd3db493b4110b924b6b6373982
Sylvester <34459277+sylvestertarnowski@users.noreply.github.com>
2019-11-16 14:05:07 +0100

    add react router as a wrapper for the whole app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Menu from './components/menu/Menu';
 import Navbar from './components/navbar/Navbar';
+import Create from './components/create/Create';
+import Learn from './components/learn/Learn';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Router>
         <Switch>
-          <Route path="/" component={Menu} />
-          <Route path="/create">{/* <Create /> */}</Route>
-          <Route path="/learn">{/* <Learn /> */}</Route>
+          <Route path="/" component={Menu} exact />
+          <Route path="/menu" component={Menu} />
+          <Route path="/create" component={Create} />
+          <Route path="/learn" component={Learn} />
         </Switch>
-      </Router>
     </div>
   );
 }
