955e1017e95e623eeb8d3d1a21d97adef4242159
Sylvester <34459277+sylvestertarnowski@users.noreply.github.com>
2019-11-16 14:27:37 +0100

    add basic collection creation actions

---
+++ src/store/actions.js
@@ -0,0 +1,31 @@
+export const ADD_WORD = 'ADD_WORD';
+export const REMOVE_WORD = 'REMOVE_WORD';
+export const UPDATE_WORD = 'UPDATE_WORD';
+
+export const CollectionActions = {
+  READ_COLLECTION: 'READ_COLLECTION',
+  SAVE_COLLECTION: 'SAVE_COLLECTION',
+  EDIT_COLLECTION: 'EDIT_COLLECTION',
+  DELETE_COLLECTION: 'DELETE_COLLECTION'
+};
+
+export const addWord = word => {
+  return {
+    type: ADD_WORD,
+    payload: { word }
+  };
+};
+
+export const removeWord = id => {
+  return {
+    type: REMOVE_WORD,
+    payload: { id }
+  };
+};
+
+export const updateWord = ({ id, word }) => {
+  return {
+    type: UPDATE_WORD,
+    payload: { word, id }
+  };
+};
