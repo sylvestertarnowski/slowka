f699cb0f43bb0bb96e3cf29bd9ed88cd2ae01bd4
Sylvester <34459277+sylvestertarnowski@users.noreply.github.com>
2019-11-16 14:04:09 +0100

    change router placement to index.js, change menu link

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,25 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Navbar.css';
-import { Link, BrowserRouter as Router } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+export const Navbar = ({ menu, create, learn }) => {
   return (
-    <Router>
-      <nav className="Navbar">
-        <ul>
-          <li>
-            <Link to="/">menu</Link>
-          </li>
-          <li>
-            <Link to="/create">create</Link>
-          </li>
-          <li>
-            <Link to="/learn">learn</Link>
-          </li>
-        </ul>
-      </nav>
-    </Router>
+    <nav className="Navbar">
+      <ul>
+        <li>
+          <Link to="/menu">{menu}</Link>
+        </li>
+        <li>
+          <Link to="/create">{create}</Link>
+        </li>
+        <li>
+          <Link to="/learn">{learn}</Link>
+        </li>
+      </ul>
+    </nav>
   );
+};
+
+Navbar.propTypes = {
+  menu: PropTypes.string,
+  create: PropTypes.string,
+  learn: PropTypes.string
+};
+
+Navbar.defaultProps = {
+  menu: 'Menu',
+  create: 'Create',
+  learn: 'Learn'
 };
 
 export default Navbar;
