7b3904d02bbb713b4175656d074eafe26c9d261e
Sylvester <34459277+sylvestertarnowski@users.noreply.github.com>
2019-12-06 23:44:51 +0100

    add display of all collections

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Collection } from '../../store/newCollectionReducers';
+import { GlobalStore } from '../../store';
 
-const Menu: React.FC = () => {
+interface P {
+  collections: Collection[];
+}
+
+const Menu: React.FC<P> = ({ collections }) => {
   return (
     <div>
       <h1>Menu works!</h1>
+      {collections.map(item => {
+        const { title } = item;
+        return (
+          <div key={title}>
+            <span>{title}</span>
+            <button>
+              Pick List
+            </button>
+          </div>
+        )
+      })}
     </div>
   );
 };
 
-export default Menu;
+Menu.propTypes = {
+  collections: PropTypes.array.isRequired
+};
+
+const mapStateToProps = ({ collections }: GlobalStore) => ({
+  collections
+});
+
+export default connect(mapStateToProps)(Menu);
